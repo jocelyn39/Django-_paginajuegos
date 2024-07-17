5420f3c0c5e5b52b211029cdbe28fb764c745d16
jocelyn <joce.rivera@duocuc.cl>
2024-07-17 19:47:52 -0400

    views clientes

--- cliente/views.py
+++ cliente/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
       
-      return render(request,'cliente/index.html')
+      return render(request,'clientes/index.html')
 
