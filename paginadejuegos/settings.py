ab6a658b93b82afb9eb177f45265b8b10117cc06
jocelyn <joce.rivera@duocuc.cl>
2024-07-17 22:52:03 -0400

    arreglo al settings

--- paginadejuegos/settings.py
+++ paginadejuegos/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-46hb-b1is0#a$me)4^*41@lzdwcy7%#aaim)#^*$xj9!+ia(6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', '127.0.0.1']
+]
 
 
 # Application definition
